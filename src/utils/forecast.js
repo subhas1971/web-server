3dc8e023be5576d85d039809b4e9d9e9a6da5c6c
Subhas <subhas@3web.com>
2019-03-22 13:05:50 -0400

    Add to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const url = 'https://api.darksky.net/forecast/5ee579516d82ad5fc9f77ad1df53f85c/'
 		} else if (body.error) {
 			callback('Unable to find location', undefined)
 		} else {			
-			callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. Feels like ' +  body.currently.apparentTemperature + '. There is ' + body.currently.precipProbability + '% chance of rain. With humidity level to '+ body.currently.humidity)
+			callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. Feels like ' +  body.currently.apparentTemperature + '. The high today is '	+ body.daily.data[0].temperatureHigh + ' with a low of '+ body.daily.data[0].temperatureLow +'. There is ' + body.currently.precipProbability + '% chance of rain. With humidity level to '+ body.currently.humidity)
 		}
 	})	 
 }	
